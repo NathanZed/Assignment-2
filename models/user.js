2eec5a56497e54ddd26342238a8f8089b3051ad8
NathanZed <60272287+NathanZed@users.noreply.github.com>
2021-03-01 18:21:33 -0500

    Commit 3
    Updated user.js
    changed 'Product' to 'User'
    Added required true to age

--- models/user.js
+++ models/user.js
@@ -2,10 +2,10 @@ const Mongoose = require('mongoose');
 
 require('bob-mongoose-currency').loadType(Mongoose);
 
-module.exports = Mongoose.model('Product', new Mongoose.Schema({
-    firstName: { type: String, required: true, unique: true },
+module.exports = Mongoose.model('User', new Mongoose.Schema({
+    firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    age: { type: Number, default: 0 },
+    age: { type: Number, required: true, default: 0 },
     socialSecurity: { type: String, required: true, unique: true,},
     address: { type: String},
     phoneNumber: { type: String},
