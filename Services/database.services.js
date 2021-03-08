ac33a065e0d6389609cb3af44ebd87d4a72c0c3d
NathanZed <60272287+NathanZed@users.noreply.github.com>
2021-03-08 14:57:17 -0500

    Commit 7
    Final Finished service combined controllers into it and made one database where it calls on the server which calls an server.js.

---
+++ Services/database.services.js
@@ -0,0 +1,86 @@
+const Express = require('express');
+const bodyParser = require('body-parser');
+const Mongoose = require('mongoose');
+
+const Product = require('../models/product.js');
+const User = require('../models/user');
+
+const app = Express();
+// app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+// product
+app.getSku = async (request) => {
+  await Product.findOne({ sku: request.params.sku }).select('-_id -__v');
+};
+
+app.saveProduct = async (request) => {
+  await new Product(request.body).save();
+};
+
+app.deleteProduct = async (request, response) => {
+  response.sendStatus(await Product.deleteMany(request.query));
+};
+
+app.deleteAllProd = async (request) => {
+  await Product.deleteOne(request);
+};
+
+app.updateProduct = async (request, response) => {
+  await Product.findOneAndReplace({ request }, response, {
+    upsert: true,
+  });
+};
+
+app.updateOneProduct = async (request, response) => {
+  await Product
+    .findOneAndUpdate({ request }, response, {
+      new: true,
+    });
+};
+
+// users
+app.getSSN = async (request) => {
+  await User.findOne({ sku: request.params.sku }).select('-_id -__v');
+};
+
+app.saveUser = async (request) => {
+  await new User(request.body).save();
+};
+
+app.deleteUser = async (request, response) => {
+  response.sendStatus(await Product.deleteMany(request.query));
+};
+
+app.deleteAllUser = async (request) => {
+  await User.deleteOne(request);
+};
+
+app.updateUser = async (request, response) => {
+  await User.findOneAndReplace({ request }, response, {
+    upsert: true,
+  });
+};
+
+app.updateOneUser = async (request, response) => {
+  await User
+    .findOneAndUpdate({ request }, response, {
+      new: true,
+    });
+};
+
+(async () => {
+  await Mongoose.connect('mongodb+srv://admin:U7CWgya36gFJg57@cluster0.vnnkb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  // app.listen(8080);
+})();
+
+const db = Mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('It Connects!');
+});
--- server.js
+++ server.js
@@ -1,29 +1,16 @@
-const express = require('express');
-const BodyParser = require('body-parser');
-const Mongoose = require('mongoose');
+const Express = require('express');
+const bodyParser = require('body-parser');
+const Product = require('./Services/database.services');
+const User = require('./Services/database.services');
 
-// const Product = require('./models/product');
-// const User = require('./Routes/user.routes');
+const app = Express();
+app.use(bodyParser.json());
 
-const user = require('./Controllers/user.controller');
-
-const app = express();
-
-app.use(BodyParser.json());
-
-app.use('/User', user);
-// require('./Routes/user.routes');
-
-// app.listen(8080);
-
-// var UserService = require('../services/user.service')
-/*
-// helper function
 const doActionThatMightFailValidation = async (request, response, action) => {
   try {
     await action();
   } catch (e) {
-    response.sendStatus( // Controller?
+    response.sendStatus(
       e.code === 11000
         || e.stack.includes('ValidationError')
         || (e.reason !== undefined && e.reason.code === 'ERR_ASSERTION')
@@ -31,229 +18,128 @@ const doActionThatMightFailValidation = async (request, response, action) => {
     );
   }
 };
-/*
-app.get('/User', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.json(await User.find(request.query).select('-_id -__v'));
-  });
-});
 
-app.get('/User/:SSN', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    const getResult = await User.findOne({ sku: request.params.sku }).select('-_id -__v');
-    if (getResult != null) {
-      response.json(getResult);
-    } else {
-      response.sendStatus(404);
-    }
-  });
-});
-/*
-app.post('/User', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    await new User(request.body).save();
-    response.sendStatus(201);
-  });
-});
-
-app.delete('/user', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.sendStatus((await User.deleteMany(request.query)).deletedCount > 0 ? 200 : 404);
-  });
-});
-
-app.delete('/products/:sku', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.sendStatus((await User.deleteOne({
-      sku: request.params.sku,
-    })).deletedCount > 0 ? 200 : 404);
-  });
-});
-
-app.put('/products/:sku', async (request, response) => {
-  const { ssn } = request.params;
-  const user = request.body;
-  user.ssn = ssn;
-  await doActionThatMightFailValidation(request, response, async () => {
-    await User.findOneAndReplace({ ssn }, user, {
-      upsert: true,
+// Product
+app.route('/products')
+  .get(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      response.json();
     });
-    response.sendStatus(200);
+  })
+  .post(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      // await new Product(request.body).save();
+      Product.saveProduct();
+      response.sendStatus(201);
+    });
+  })
+  .delete(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      response.sendStatus(await Product.deleteProduct > 0 ? 200 : 404);
+    });
   });
-});
 
-app.patch('/products/:sku', async (request, response) => {
-  const { SSN } = request.params;
-  const user = request.body;
-  delete user.SSN;
-  await doActionThatMightFailValidation(request, response, async () => {
-    const patchResult = await User
-        .findOneAndUpdate({ ssn }, user, {
-          new: true,
-        })
+app.route('/product/:sku')
+  .get(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      Product.getSku(request);
+      const getResult = request;
+      if (getResult != null) {
+        response.json(getResult);
+      } else {
+        response.sendStatus(404);
+      }
+    });
+  })
+  .delete(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      response.sendStatus((await Product.deleteAllProd(request)({
+        sku: request.params.sku,
+      })).deletedCount > 0 ? 200 : 404);
+    });
+  })
+  .put('/products/:sku', async (request, response) => {
+    const { sku } = request.params;
+    const product = request.body;
+    product.sku = sku;
+    await doActionThatMightFailValidation(request, response, async () => {
+      Product.updateProduct(request);
+      response.sendStatus(200);
+    });
+  })
+  .patch('/products/:sku', async (request, response) => {
+    const { sku } = request.params;
+    const product = request.body;
+    delete product.sku;
+    await doActionThatMightFailValidation(request, response, async () => {
+      const patchResult = await Product.updateOneProduct(sku)
         .select('-_id -__v');
-    if (patchResult != null) {
-      response.json(patchResult);
-    } else {
-      response.sendStatus(404);
-    }
-  });
-*/
-
-/*
-app.get('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.json(await User.find(request.query).select('-_id -__v'));
-  });
-});
-
-app.get('/products/:sku', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    const getResult = await User.findOne({ sku: request.params.sku }).select('-_id -__v');
-    if (getResult != null) {
-      response.json(getResult);
-    } else {
-      response.sendStatus(404);
-    }
-  });
-});
-
-app.post('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    await new User(request.body).save();
-    response.sendStatus(201);
-  });
-});
-/*
-app.delete('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.sendStatus((await User.deleteMany(request.query)).deletedCount > 0 ? 200 : 404);
-  });
-});
-
-app.delete('/products/:sku', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.sendStatus((await User.deleteOne({
-      sku: request.params.sku,
-    })).deletedCount > 0 ? 200 : 404);
-  });
-});
-
-app.put('/products/:sku', async (request, response) => {
-  const { sku } = request.params;
-  const product = request.body;
-  product.sku = sku;
-  await doActionThatMightFailValidation(request, response, async () => {
-    await User.findOneAndReplace({ sku }, product, {
-      upsert: true,
+      if (patchResult != null) {
+        response.json(patchResult);
+      } else {
+        response.sendStatus(404);
+      }
     });
-    response.sendStatus(200);
   });
-});
 
-app.patch('/products/:sku', async (request, response) => {
-  const { sku } = request.params;
-  const product = request.body;
-  delete product.sku;
-  await doActionThatMightFailValidation(request, response, async () => {
-    const patchResult = await User
-      .findOneAndUpdate({ sku }, product, {
-        new: true,
-      })
-      .select('-_id -__v');
-    if (patchResult != null) {
-      response.json(patchResult);
-    } else {
-      response.sendStatus(404);
-    }
-  });
-});
-*/
-/*
-// different service product
-app.get('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.json(await Product.find(request.query).select('-_id -__v'));
-  });
-});
-
-app.get('/products/:sku', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    const getResult = await Product.findOne({ sku: request.params.sku }).select('-_id -__v');
-    if (getResult != null) {
-      response.json(getResult);
-    } else {
-      response.sendStatus(404);
-    }
-  });
-});
-
-app.post('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    await new Product(request.body).save();
-    response.sendStatus(201);
-  });
-});
-
-app.delete('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.sendStatus((await Product.deleteMany(request.query)).deletedCount > 0 ? 200 : 404);
-  });
-});
-
-app.delete('/products/:sku', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.sendStatus((await Product.deleteOne({
-      sku: request.params.sku,
-    })).deletedCount > 0 ? 200 : 404);
-  });
-});
-
-app.put('/products/:sku', async (request, response) => {
-  const { sku } = request.params;
-  const product = request.body;
-  product.sku = sku;
-  await doActionThatMightFailValidation(request, response, async () => {
-    await Product.findOneAndReplace({ sku }, product, {
-      upsert: true,
+// User
+app.route('/user')
+  .get(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      response.json();
     });
-    response.sendStatus(200);
+  })
+  .post(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      User.saveUser();
+      response.sendStatus(201);
+    });
+  })
+  .delete(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      response.sendStatus(await User.deleteUser > 0 ? 200 : 404);
+    });
   });
-});
 
-app.patch('/products/:sku', async (request, response) => {
-  const { sku } = request.params;
-  const product = request.body;
-  delete product.sku;
-  await doActionThatMightFailValidation(request, response, async () => {
-    const patchResult = await Product
-      .findOneAndUpdate({ sku }, product, {
-        new: true,
-      })
-      .select('-_id -__v');
-    if (patchResult != null) {
-      response.json(patchResult);
-    } else {
-      response.sendStatus(404);
-    }
+app.route('/user/:ssn')
+  .get(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      Product.getSSN(request);
+      const getResult = request;
+      if (getResult != null) {
+        response.json(getResult);
+      } else {
+        response.sendStatus(404);
+      }
+    });
+  })
+  .delete(async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+      response.sendStatus((await Product.deleteAllUser({
+        sku: request.params.sku,
+      })).deletedCount > 0 ? 200 : 404);
+    });
+  })
+  .put(async (request, response) => {
+    const { ssn } = request.params;
+    const user = request.body;
+    user.ssn = ssn;
+    await doActionThatMightFailValidation(request, response, async () => {
+      Product.updateUser(ssn);
+      response.sendStatus(200);
+    });
+  })
+  .patch(async (request, response) => {
+    const { ssn } = request.params;
+    const user = request.body;
+    delete user.sku;
+    await doActionThatMightFailValidation(request, response, async () => {
+      const patchResult = Product.updateOneUser(ssn)
+        .select('-_id -__v');
+      if (patchResult != null) {
+        response.json(patchResult);
+      } else {
+        response.sendStatus(404);
+      }
+    });
   });
-});
-*/
-(async () => {
-  await Mongoose.connect('mongodb+srv://ComAd:Admin@database.n8bk7.mongodb.net/Database?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  });
-  // app.listen(8080);
-})();
-
-const db = Mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('It Connects!');
-});
-require('./Routes/user.routes');
-
-app.listen(8080);
